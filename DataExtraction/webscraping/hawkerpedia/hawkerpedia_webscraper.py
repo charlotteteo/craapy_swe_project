c35419653a5adc6d23e52f00dc5a3b7282814d2a
Charlotte Teo <charlotteteo.ct@gmail.com>
2022-01-26 17:31:24 +0800

    change directory + cleaned hawker closure data

--- DataExtraction/webscraping/hawkerpedia/hawkerpedia_webscraper.py
+++ DataExtraction/webscraping/hawkerpedia/hawkerpedia_webscraper.py
@@ -4,7 +4,7 @@ import csv
 import pandas as pd
 from bs4 import BeautifulSoup
 
-
+#print(content_list[0].split("https://www.hawkerpedia.com.sg/en/hawker-centre/")[1])
 from urllib.request import urlopen
 import regex as re
 html = urlopen("https://www.hawkerpedia.com.sg/en/directories")
@@ -12,13 +12,15 @@ text = html.read()
 plaintext = text.decode('utf8')
 links = re.findall("href=[\"\'](/en/hawker-centre/)(.*?)[\"\']", plaintext)
 # print(links)
+
+
 #generate links
 for x in range(len(links)):
     # tail+=links[x][1]
-    print(links[x][1])
+    # print(links[x][1])
     try: 
         actual_link="https://www.hawkerpedia.com.sg/en/hawker-centre/"+links[x][1]
-        print(actual_link)
+        # print(actual_link)
         # print(convertedDict["Name"])
         name="output/"+links[x][1]
         # full dataset
@@ -32,19 +34,20 @@ for x in range(len(links)):
     for x in range(len(all_info.split("stallList",1)[1].split("[]},"))):
         try:
             split_further=all_info.split("stallList",1)[1].split("[]},")
-            # print(split_further[x])
-        
-            # break string based on ""Photo":[]},""
+           
             edited=split_further[x]+'" "}'
             edited=edited.split('"Id"')[1]
 
             edited='{"Id"'+edited
         
             convertedDict=json.loads(edited)
-     
-            # print(convertedDict["Name"])
-            stores[str(x)]=convertedDict
-    
+            importantKey=['Id', 'Thumbnail', 'Name', 'Rating', 'BusinessName', 'UnitNo',
+        'PhoneNumber', 'OperationHours', 'CommunityRating',
+       'HighlightItems', 'FoodCategories']
+            finalDict={key: convertedDict[key] for key in importantKey}
+            stores[str(x)]=finalDict
+            
+      
             
         except:
             break
@@ -56,4 +59,3 @@ for x in range(len(links)):
     
     pdObj = pd.read_json("json/"+name+'.json', orient='index')
     pdObj.to_csv(name+".csv")
-
